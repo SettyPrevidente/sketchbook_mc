4ae428a679f0b25b8bce5ac06417d672f645ce0b
VladislavImp <vladislavimper92@gmail.com>
2022-12-14 19:55:04 +0700

    add class of Tecnical_CraftTable

---
+++ src/main/java/com/setty/Tfc_additionsMod/Tecnical_CraftTable.java
@@ -0,0 +1,25 @@
+package com.setty.Tfc_additionsMod;
+
+import com.google.common.collect.ImmutableList;
+import com.google.errorprone.annotations.Immutable;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.Material;
+import net.minecraft.world.level.storage.loot.LootContext;
+import net.minecraftforge.client.event.ColorHandlerEvent;
+
+public class Tecnical_CraftTable extends Block
+{
+    public Tecnical_CraftTable()
+    {
+        super(Properties.create(Material.WOOD).harvestTool(ToolType.AXE));
+    }
+
+    @Override
+    public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder)
+    {
+        return ImmutableList.of(new ItemStack(Items.CAKE, 8), new ItemStack(Items.TORCH, 1)
+    }
+}
